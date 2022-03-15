feebc43ea3134ca42979c3bbe03a20b3e4f3347c
Md Hosne Mobarok <59361903+hosnemobarok@users.noreply.github.com>
2022-03-15 10:44:34 +0600

    Add files via upload

---
+++ ToDoListRealm/TodoListRealmDatabase/TodoListRealmDatabase/Model/DatabaseHelper.swift
@@ -0,0 +1,45 @@
+//
+//  DatabaseHelper.swift
+//  ToDoListRealm
+//
+//  Created by Md Hosne Mobarok on 15/3/22.
+//
+
+import RealmSwift
+import UIKit
+
+class DatabaseHelper{
+    
+    static let shared = DatabaseHelper()
+    /// Open the local-only default realm
+    private var realm = try! Realm()
+    
+    func getDatabasePath() -> URL?{
+        return Realm.Configuration.defaultConfiguration.fileURL
+    }
+    
+    func saveContact(contact: Contact){
+        try! realm.write({
+            realm.add(contact)
+        })
+    }
+    
+    func updateContact(oldContact: Contact, newContact: Contact){
+        try! realm.write{
+            //Yogesh = Pratik(BOSS)
+            oldContact.firstname = newContact.firstname
+            oldContact.lastname = newContact.lastname
+        }
+    }
+    
+    func deleteContact(contact: Contact){
+        try! realm.write{
+            realm.delete(contact)
+        }
+    }
+    
+    func getAllContacts() -> [Contact]{
+        return Array(realm.objects(Contact.self))
+    }
+    
+}
